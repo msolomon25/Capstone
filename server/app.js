de12e56eebb609d7ac19b32a440e64ae3f8f043d
Madori Solomon <madorisolomon@gmail.com>
2023-12-29 18:58:08 -0600

    commented out mongoose stuff for next weeks homework

--- server/app.js
+++ server/app.js
@@ -8,19 +8,19 @@ dotenv.config();
 const app = express();
 
 //Mongoose library
-mongoose.connect(process.env.MONGODB, {
-  // Configuration options to remove deprecation warnings, just include them to remove clutter
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+// mongoose.connect(process.env.MONGODB, {
+//   // Configuration options to remove deprecation warnings, just include them to remove clutter
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
 
-const db = mongoose.connection;
+// const db = mongoose.connection;
 
-db.on("error", console.error.bind(console, "Connection Error:"));
-db.once(
-  "open",
-  console.log.bind(console, "Successfully opened connection to Mongo!")
-);
+// db.on("error", console.error.bind(console, "Connection Error:"));
+// db.once(
+//   "open",
+//   console.log.bind(console, "Successfully opened connection to Mongo!")
+// );
 
 //logging middleware
 const logging = (request, response, next) => {
