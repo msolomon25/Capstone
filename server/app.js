8eecb18247df35f64be9ad46baeefd3cc0848ddc
Madori Solomon <madorisolomon@gmail.com>
2024-01-04 20:15:23 -0600

    added routers for prompt api

--- server/app.js
+++ server/app.js
@@ -2,25 +2,26 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import prompt from "./routers/prompt.js";
 
 dotenv.config();
 // Initialize the Express application
 const app = express();
 
-//Mongoose library
-// mongoose.connect(process.env.MONGODB, {
-//   // Configuration options to remove deprecation warnings, just include them to remove clutter
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// });
+// Mongoose library
+mongoose.connect(process.env.MONGODB, {
+  // Configuration options to remove deprecation warnings, just include them to remove clutter
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
-// const db = mongoose.connection;
+const db = mongoose.connection;
 
-// db.on("error", console.error.bind(console, "Connection Error:"));
-// db.once(
-//   "open",
-//   console.log.bind(console, "Successfully opened connection to Mongo!")
-// );
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
 
 //logging middleware
 const logging = (request, response, next) => {
@@ -58,4 +59,5 @@ app.get("/status", (request, response) => {
 
 // Tell the Express app to start listening
 // Let the humans know I am running and listening on 4040
+app.use("/prompt", prompt);
 app.listen(4040, () => console.log("Listening on port 4040"));
